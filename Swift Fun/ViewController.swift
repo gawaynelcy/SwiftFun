8367f52022ec3b37307530e81910b2f4da159282
Chin Yun Lau <gawaynelcy@gmail.com>
2017-12-16 15:05:29 +0800

    my first commit as gitters!

--- Swift Fun/ViewController.swift
+++ Swift Fun/ViewController.swift
@@ -10,9 +10,46 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var buttonCount = 0
+    @IBOutlet weak var myLabel: UILabel!
+    
+    @IBAction func myButton(_ sender: Any) {
+        
+        
+        
+        buttonCount = buttonCount + 1
+        print(buttonCount)
+
+        if buttonCount == 1 {
+            view.backgroundColor = UIColor.green
+            myLabel.text = "Keep hitting me!"
+        }
+        if buttonCount == 5 {
+            view.backgroundColor = UIColor.yellow
+            myLabel.text = "Ahhhh... Getting there! Keep Hitting!!!"
+        }
+        if buttonCount == 10 {
+            view.backgroundColor = UIColor.red
+            myLabel.text = "Yun is disappointed!"
+        }
+        if buttonCount == 11 {
+            view.backgroundColor = UIColor.cyan
+            myLabel.text = "Okay! STOP! You are getting on my nerves!"
+        }
+        if buttonCount == 15 {
+            view.backgroundColor = UIColor.blue
+            myLabel.text = "BACK OFF!!!"
+        }
+
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+
+        
     }
 
     override func didReceiveMemoryWarning() {
